674d2e158ec5cb4e0db43a9941755516810398a8
ebcho <joeunbi514@naver.com>
2024-02-05 09:48:30 +0900

    build : 프로젝트 초기 설정

---
+++ src/main/java/com/i6/honterview/HonterviewApplication.java
@@ -0,0 +1,13 @@
+package com.i6.honterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HonterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HonterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/i6/honterview/HonterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.i6.honterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HonterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
