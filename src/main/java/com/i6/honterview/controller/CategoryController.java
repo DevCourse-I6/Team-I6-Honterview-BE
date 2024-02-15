2d960bd1c605a60828c9239b878137c98bb36ebf
ebcho <joeunbi514@naver.com>
2024-02-15 13:47:52 +0900

    feat: 카테고리 CUD

--- src/main/java/com/i6/honterview/controller/CategoryController.java
+++ src/main/java/com/i6/honterview/controller/CategoryController.java
@@ -1,16 +1,26 @@
 package com.i6.honterview.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.i6.honterview.dto.request.CategoryCreateRequest;
+import com.i6.honterview.dto.request.CategoryUpdateRequest;
 import com.i6.honterview.dto.response.CategoryResponse;
 import com.i6.honterview.response.ApiResponse;
 import com.i6.honterview.service.CategoryService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -25,4 +35,29 @@ public class CategoryController {
 		List<CategoryResponse> response = categoryService.getCategories();
 		return ResponseEntity.ok(ApiResponse.ok(response));
 	}
+
+	@PostMapping
+	public ResponseEntity<ApiResponse> createCategory(@Valid @RequestBody CategoryCreateRequest request) {
+		Long id = categoryService.createCategory(request);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequestUri()
+			.path("/{id}")
+			.buildAndExpand(id)
+			.toUri();
+		return ResponseEntity.created(location).body(ApiResponse.created(id));
+	}
+
+	@PatchMapping("/{id}")
+	public ResponseEntity<ApiResponse> updateCategory(
+		@PathVariable Long id,
+		@Valid @RequestBody CategoryUpdateRequest request) {
+		CategoryResponse response = categoryService.updateCategory(id, request);
+		return ResponseEntity.ok(ApiResponse.ok(response));
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<ApiResponse> deleteCategory(@PathVariable Long id) {
+		categoryService.deleteCategory(id);
+		return ResponseEntity.noContent().build();
+	}
+
 }
--- src/main/java/com/i6/honterview/service/CategoryService.java
+++ src/main/java/com/i6/honterview/service/CategoryService.java
@@ -3,14 +3,20 @@ package com.i6.honterview.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.i6.honterview.domain.Category;
+import com.i6.honterview.dto.request.CategoryCreateRequest;
+import com.i6.honterview.dto.request.CategoryUpdateRequest;
 import com.i6.honterview.dto.response.CategoryResponse;
+import com.i6.honterview.exception.CustomException;
+import com.i6.honterview.exception.ErrorCode;
 import com.i6.honterview.repository.CategoryRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class CategoryService {
 	private final CategoryRepository categoryRepository;
@@ -18,5 +24,25 @@ public class CategoryService {
 	public List<CategoryResponse> getCategories() {
 		List<Category> categories = categoryRepository.findAll();
 		return categories.stream().map(CategoryResponse::from).toList();
+	}
+
+	public Long createCategory(CategoryCreateRequest request) {
+		Category category = categoryRepository.save(request.toEntity());
+		return category.getId();
+	}
+
+	public CategoryResponse updateCategory(Long id, CategoryUpdateRequest request) {
+		Category category = categoryRepository.findById(id)
+			.orElseThrow(() -> new CustomException(ErrorCode.CATEGORY_NOT_FOUND));
+
+		category.changeCategoryName(request.categoryName());
+		category = categoryRepository.save(category);
+		return CategoryResponse.from(category);
+	}
+
+	public void deleteCategory(Long id) {
+		Category category = categoryRepository.findById(id)
+			.orElseThrow(() -> new CustomException(ErrorCode.CATEGORY_NOT_FOUND));
+		categoryRepository.delete(category);
 	}
 }
