b57067b9cf175703835a5221fb0b6a98657f0944
조은비 <69096886+eunbc@users.noreply.github.com>
2024-02-15 20:50:23 +0900

    Revert "feat: 카테고리 CUD (#20)"
    This reverts commit def0ed5654b5c1eff95a061ca1b8e6f6d24cdc6b.

--- src/main/java/com/i6/honterview/domain/Category.java
+++ src/main/java/com/i6/honterview/domain/Category.java
@@ -1,16 +1,10 @@
 package com.i6.honterview.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -28,15 +22,4 @@ public class Category extends BaseEntity {
 
 	@Column(name = "category_name", nullable = false)
 	private String categoryName;
-
-	@OneToMany(mappedBy = "category", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
-	private List<QuestionCategory> questionCategories = new ArrayList<>();
-
-	public Category(String categoryName) {
-		this.categoryName = categoryName;
-	}
-
-	public void changeCategoryName(String categoryName) {
-		this.categoryName = categoryName;
-	}
 }
