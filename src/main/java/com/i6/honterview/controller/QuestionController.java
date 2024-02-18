4271ff80ccda614580579b4b1005d8e0a30939b0
ebcho <joeunbi514@naver.com>
2024-02-19 08:05:35 +0900

    docs: Question, Category 컨트롤러 스웨거 작성

--- src/main/java/com/i6/honterview/controller/QuestionController.java
+++ src/main/java/com/i6/honterview/controller/QuestionController.java
@@ -22,9 +22,12 @@ import com.i6.honterview.response.ApiResponse;
 import com.i6.honterview.service.QuestionHeartService;
 import com.i6.honterview.service.QuestionService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "질문")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/questions")
@@ -33,8 +36,9 @@ public class QuestionController {
 	private final QuestionService questionService;
 	private final QuestionHeartService questionHeartService;
 
+	@Operation(summary = "질문 목록 조회")
 	@GetMapping
-	public ResponseEntity<ApiResponse> getQuestions(
+	public ResponseEntity<ApiResponse<PageResponse<QuestionResponse>>> getQuestions(
 		@RequestParam(value = "page", defaultValue = "1") int page,
 		@RequestParam(value = "size", defaultValue = "5") int size,
 		@RequestParam(value = "query", required = false) String query,
@@ -46,8 +50,9 @@ public class QuestionController {
 		return ResponseEntity.ok(ApiResponse.ok(response));
 	}
 
+	@Operation(summary = "질문 상세 조회")
 	@GetMapping("/{id}")
-	public ResponseEntity<ApiResponse> getQuestionById(
+	public ResponseEntity<ApiResponse<QuestionDetailResponse>> getQuestionById(
 		@PathVariable Long id,
 		@RequestParam(value = "page", defaultValue = "1") int page,
 		@RequestParam(value = "size", defaultValue = "5") int size
@@ -56,21 +61,24 @@ public class QuestionController {
 		return ResponseEntity.ok(ApiResponse.ok(response));
 	}
 
+	@Operation(summary = "질문 수정")
 	@PatchMapping("/{id}")
-	public ResponseEntity<ApiResponse> updateQuestion(
+	public ResponseEntity<Void> updateQuestion(
 		@PathVariable Long id, @Valid @RequestBody QuestionUpdateRequest request) {
 		questionService.updateQuestion(id, request);
 		return ResponseEntity.noContent().build();
 	}
 
+	@Operation(summary = "질문 삭제")
 	@DeleteMapping("/{id}")
-	public ResponseEntity<ApiResponse> deleteQuestion(@PathVariable Long id) {
+	public ResponseEntity<Void> deleteQuestion(@PathVariable Long id) {
 		questionService.deleteQuestion(id);
 		return ResponseEntity.noContent().build();
 	}
 
+	@Operation(summary = "질문 좋아요/좋아요 취소")
 	@PostMapping("/{id}/hearts")
-	public ResponseEntity<ApiResponse> clickQuestionHeart(@PathVariable Long id) {
+	public ResponseEntity<ApiResponse<QuestionHeartClickResponse>> clickQuestionHeart(@PathVariable Long id) {
 		QuestionHeartClickResponse response = questionHeartService.clickQuestionHeart(id, 1L); //TODO: 회원 연동
 		return ResponseEntity.ok(ApiResponse.ok(response));
 	}
