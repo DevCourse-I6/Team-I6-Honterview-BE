9282b4fb1960c208a5226b672a5da40a9d4076c2
ebcho <joeunbi514@naver.com>
2024-03-01 23:45:39 +0900

    fix: 질문 목록, 질문 상세에 좋아요 수 추가

--- src/main/java/com/i6/honterview/dto/response/AnswerResponse.java
+++ src/main/java/com/i6/honterview/dto/response/AnswerResponse.java
@@ -8,11 +8,22 @@ import io.swagger.v3.oas.annotations.media.Schema;
 public record AnswerResponse(
 	@Schema(description = "답변 id", example = "123")
 	Long id,
+
 	@Schema(description = "답변 내용", example = "JVM은 플랫폼 독립적인 코드 실행을 가능하게 합니다.")
 	String content,
+
 	@Schema(description = "답변 작성자", example = "라이언")
-	String nickname) {
+	String nickname,
+
+	@Schema(description = "좋아요 수")
+	int heartsCount
+
+) {
 	public static AnswerResponse from(Answer answer) {
-		return new AnswerResponse(answer.getId(), answer.getContent(), answer.getMember().getNickname());
+		return new AnswerResponse(
+			answer.getId(),
+			answer.getContent(),
+			answer.getMember().getNickname(),
+			answer.getAnswerHearts().size());
 	}
 }
