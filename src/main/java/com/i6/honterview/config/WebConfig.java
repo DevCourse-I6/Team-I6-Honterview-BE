0fd734c55abe12b8aa853c66b003d6899e325c4a
byulcode <bybit0408@gmail.com>
2024-03-06 09:54:21 +0900

    feat: currentAccount 어노테이션 생성 및 등록

--- src/main/java/com/i6/honterview/config/WebConfig.java
+++ src/main/java/com/i6/honterview/config/WebConfig.java
@@ -1,13 +1,27 @@
 package com.i6.honterview.config;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.i6.honterview.security.resolver.CurrentAccountArgumentResolver;
+
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+	private final CurrentAccountArgumentResolver currentAccountArgumentResolver;
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(currentAccountArgumentResolver);
+	}
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
