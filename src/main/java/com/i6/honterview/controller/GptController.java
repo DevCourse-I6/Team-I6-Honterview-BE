a4b1d1d9354fb46df88d542b55c36287bdefddad
byulcode <bybit0408@gmail.com>
2024-03-08 13:47:59 +0900

    feat: 질문 재생성 생성 기능 구현 (#105)

--- src/main/java/com/i6/honterview/controller/GptController.java
+++ src/main/java/com/i6/honterview/controller/GptController.java
@@ -10,11 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.i6.honterview.dto.request.GptNewQuestionCreateRequest;
 import com.i6.honterview.dto.request.GptQuestionCreateRequest;
 import com.i6.honterview.dto.response.GptQuestionCreateResponse;
 import com.i6.honterview.response.ApiResponse;
 import com.i6.honterview.service.GptService;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -27,12 +29,27 @@ public class GptController {
 
 	private final GptService gptService;
 
+	@Operation(summary = "GPT 꼬리질문 생성(다음 질문 넘어갈 때 호출)")
 	@PostMapping("/{interviewId}")
-	public ResponseEntity<ApiResponse<GptQuestionCreateResponse>> createGptQuestion(
+	public ResponseEntity<ApiResponse<GptQuestionCreateResponse>> createTailGptQuestion(
 		@PathVariable Long interviewId,
 		@Valid @RequestBody GptQuestionCreateRequest request
 	) {
-		GptQuestionCreateResponse response = gptService.createGptQuestion(interviewId, request);
+		GptQuestionCreateResponse response = gptService.createTailGptQuestion(interviewId, request);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequestUri()
+			.path("/{id}")
+			.buildAndExpand(response.id())
+			.toUri();
+		return ResponseEntity.created(location).body(ApiResponse.created(response));
+	}
+
+	@Operation(summary = "GPT 꼬리질문 재생성")
+	@PostMapping("/{interviewId}/new")
+	public ResponseEntity<ApiResponse<GptQuestionCreateResponse>> createNewGptQuestion(
+		@PathVariable Long interviewId,
+		@Valid @RequestBody GptNewQuestionCreateRequest request
+	) {
+		GptQuestionCreateResponse response = gptService.createNewGptQuestion(interviewId, request);
 		URI location = ServletUriComponentsBuilder.fromCurrentRequestUri()
 			.path("/{id}")
 			.buildAndExpand(response.id())
