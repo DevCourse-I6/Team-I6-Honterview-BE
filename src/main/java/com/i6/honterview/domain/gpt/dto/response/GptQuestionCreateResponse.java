52f7c79ae089b7877b5e401a74dd64743860ab47
byulcode <bybit0408@gmail.com>
2024-03-22 01:43:15 +0900

    fix: 재생성된 꼬리 질문 다듬는 로직 추가

--- src/main/java/com/i6/honterview/domain/gpt/dto/response/GptQuestionCreateResponse.java
+++ src/main/java/com/i6/honterview/domain/gpt/dto/response/GptQuestionCreateResponse.java
@@ -9,7 +9,17 @@ public record GptQuestionCreateResponse(
 	@Schema(description = "생성된 꼬리질문 내용", example = "JVM은 왜 자바 바이트코드를 직접 실행시키지 않고 중간 언어로 번역해서 실행하는 것일까요?")
 	String tailQuestionContent) {
 	public static GptQuestionCreateResponse from(String id, Choice choice) {
-		String content = choice.message().content().replace("\"", "");
+		String content = refineSentence(choice.message().content());
 		return new GptQuestionCreateResponse(id, content);
+	}
+
+	private static String refineSentence(String oldSentence) {
+		String[] stringsToRemove = {"\"", "꼬리 질문", ":"};
+		String newSentence = oldSentence;
+
+		for (String str : stringsToRemove) {
+			newSentence = newSentence.replace(str, "");
+		}
+		return newSentence;
 	}
 }
